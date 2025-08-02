35f8583f5e0447e4f84e079b75d657f8e17b1800
Simon <Simon.Sleeeep@outlook.com>
2025-08-02 13:57:06 +0800

    refactor: Refactor whole architecture

---
+++ app/src/main/kotlin/com/mocharealm/accompanist/domain/repository/MusicRepository.kt
@@ -0,0 +1,9 @@
+package com.mocharealm.accompanist.domain.repository
+
+import com.mocharealm.accompanist.domain.model.MusicItem
+import com.mocharealm.accompanist.lyrics.model.SyncedLyrics
+
+interface MusicRepository {
+    suspend fun getMusicItems(): List<MusicItem>
+    suspend fun getLyricsFor(item: MusicItem): SyncedLyrics?
+}
