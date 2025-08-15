a0f4888bdd9ec00054bdf91ac456d1a30e2f8a56
Simon <Simon.Sleeeep@outlook.com>
2025-08-15 10:21:33 +0800

    refactor: Update SFPro font to use variable font

--- src/src/main/kotlin/com/mocharealm/accompanist/lyrics/ui/theme/Type.kt
+++ src/src/main/kotlin/com/mocharealm/accompanist/lyrics/ui/theme/Type.kt
@@ -1,16 +1,61 @@
 package com.mocharealm.accompanist.lyrics.ui.theme
 
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import com.mocharealm.accompanist.lyrics.ui.R
 
+@OptIn(ExperimentalTextApi::class)
 val SFPro = FontFamily(
-    Font(resId = R.font.sf_pro_display_bold, weight = FontWeight.Bold),
-    Font(resId = R.font.sf_pro_display_regular, weight = FontWeight.Light),
-    Font(resId = R.font.sf_pro_display_medium, weight = FontWeight.Medium),
-    Font(resId = R.font.sf_pro_display_black, weight = FontWeight.Black),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.ExtraLight,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.ExtraLight.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.Light,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Light.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.Medium,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Medium.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.SemiBold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.SemiBold.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.Bold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Bold.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.ExtraBold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.ExtraBold.weight),
+        )
+    ),
+    Font(
+        resId = R.font.sf_pro,
+        weight = FontWeight.Black,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Black.weight),
+        )
+    ),
 )
-
-
-
